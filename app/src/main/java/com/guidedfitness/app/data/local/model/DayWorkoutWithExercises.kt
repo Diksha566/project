df06d6809fdf2e5bbd6acb06b4d933db2516d947
Diksha Jethwa <121545576+Diksha566@users.noreply.github.com>
2026-03-17 18:11:32 +0530

    bug fix

--- app/src/main/java/com/guidedfitness/app/data/local/model/DayWorkoutWithExercises.kt
+++ app/src/main/java/com/guidedfitness/app/data/local/model/DayWorkoutWithExercises.kt
@@ -9,25 +9,8 @@ data class DayWorkoutWithExercises(
     @Embedded val workout: DayWorkoutEntity,
     @Relation(
         parentColumn = "workoutId",
-        entityColumn = "workoutId"
-    )
-    val exercises: List<ExerciseEntity>
-)
-
-package com.guidedfitness.app.data.local.model
-
-import androidx.room.Embedded
-import androidx.room.Relation
-import com.guidedfitness.app.data.local.entity.DayWorkoutEntity
-import com.guidedfitness.app.data.local.entity.ExerciseEntity
-
-data class DayWorkoutWithExercises(
-    @Embedded val workout: DayWorkoutEntity,
-    @Relation(
-        parentColumn = "workoutId",
         entityColumn = "workoutId",
         entity = ExerciseEntity::class
     )
     val exercises: List<ExerciseEntity>
 )
-
